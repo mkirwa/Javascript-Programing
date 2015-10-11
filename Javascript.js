4391514c10dc4d859ce2b39af7826eef267517ca
mkirwa <mahlonkibiwott@gmail.com>
2015-10-11 00:13:46 -0400

    changes

--- Javascript.js
+++ Javascript.js
@@ -38,3 +38,26 @@ for (var i = 0; i < cities.length; i++) {
     console.log("I would like to visit " + cities[i]);
 }
 
+//Code that checks the text for the first letter of your name and adds then adds the number of character equal to your name
+
+
+var myName = "Eric";
+var hits = [];
+
+// Look for "E" in the text
+for(var i = 0; i < text.length; i++) {
+    if (text[i] === "E") {
+        // If we find it, add characters up to
+        // the length of my name to the array
+        for(var j = i; j < (myName.length + i); j++) {
+            hits.push(text[j]);
+        }
+    }
+}
+
+if (hits.length === 0) {
+    console.log("Your name wasn't found!");
+} else {
+    console.log(hits);
+}
+
