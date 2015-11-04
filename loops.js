2b13639be3a8d3a7fb2232d052b80c5c6f901ec3
mkirwa <mahlonkibiwott@gmail.com>
2015-11-03 21:39:49 -0500

    After adding loops.js

---
+++ loops.js
@@ -0,0 +1,41 @@
+
+//while loops and for loops in javascript
+
+var soloLoop = function(){
+  //Your code goes here!
+  
+  var mahlon = true;
+  
+  while(mahlon)
+  {
+    console.log("Looped once!");
+    
+    mahlon = false;
+  }
+  
+};
+
+soloLoop();
+
+//for loops and while loops in javascript
+
+//for loops in javascript
+function forloops(looping)
+{
+    var mahlon = true;
+    for(var i=0; i<4;i++)
+    
+    {
+        while(mahlon){
+            
+        console.log("a simple print statement");
+        var mahlon = false
+        }
+    }
+    
+   
+    
+}
+forloops();
+
+
