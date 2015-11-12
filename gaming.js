0461fa5fb293c4cbb5f4a8d8c546e5c53c9b9542
mkirwa <mahlonkibiwott@gmail.com>
2015-11-11 23:35:06 -0500

    changes

--- gaming.js
+++ gaming.js
@@ -81,5 +81,15 @@ while (slaying) {
     slaying = false;
   }
 }
+//coding lines
 
+var slaying= true;
+var youHit=Math.floor(Math.random()*2);
+var damageThisRound= Math.floor(Math.random()*5+1);
+var totalDamage=0;
+while(slaying)
+{
+    
+    slaying= false;
+}
 
